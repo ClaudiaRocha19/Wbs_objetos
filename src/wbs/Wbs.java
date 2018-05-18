d0f79050262a6da7ba3cc6cfd0cf761d9fd1d28d
rpayan <rpayan@SU10-40.uninorteacad.uninorte.edu.co>
2018-05-18 08:28:03 -0500

    prueba de commit

--- src/wbs/Wbs.java
+++ src/wbs/Wbs.java
@@ -15,7 +15,10 @@ public class Wbs {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("this 2");
+        
     }
+    
+    
+    
     
 }
